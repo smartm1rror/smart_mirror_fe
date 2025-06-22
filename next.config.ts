906685c9bf1956fff83ed1acf7a6527b8a5275e0
csy <tnehd83@naver.com>
2025-06-23 01:27:50 +0900

    feat:cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['192.168.219.200'],
+
+  // 또는 모든 오리진 허용 (개발 환경에서만)
+  experimental: {
+    allowedDevOrigins: ['*']
+  }
 };
 
 export default nextConfig;
